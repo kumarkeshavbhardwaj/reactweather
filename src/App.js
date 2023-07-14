70567378d482da9c10ffa06c402136f75cd6482d
kumarkeshavbhardwaj <kumarkeshavbhardwaj@gmail.com>
2023-07-14 12:18:28 +0530

    Removed error overlay

--- src/App.js
+++ src/App.js
@@ -13,21 +13,23 @@ function App () {
   const [temp, setTemp] = useState([]);
   const [weatherType, setWeatherType] = useState([]);
   const [typedCity, setTypedCity] = useState('New Delhi');
+  const [data, setData] = useState(null);
 
-  const getWeather = () => {
-    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${typedCity}&units=metric&appid=${API_KEY}`).then((response) => {
-     console.log(response.data.name)
-     setTemp(response.data.main)
-     setCity(response.data.name)
-     setWeatherType(response.data.weather[0].main)
-   })
+  const getWeather = async () => {
+    const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${typedCity}&units=metric&appid=${API_KEY}`);
+   if(res.status===200){
+    console.log(res.data.name)
+    setTemp(res.data.main)
+    setCity(res.data.name)
+    setWeatherType(res.data.weather[0].main)
+    setData(res.data)
+   }
  }
   
 
   useEffect(()=> {
   getWeather();
 
-  
   }, []);
 
 
@@ -43,7 +45,8 @@ function App () {
   return(
     
   <>
-  
+
+  <div> 
   <div className='input-container'>
     <input onKeyDown={handleKeyPress} value={inputValue} onChange={(e) => {
       setInputValue(e.target.value)
@@ -64,6 +67,7 @@ function App () {
                  <span>{temp.temp_min}<sup>&deg;</sup>C</span>
             </div>
         </div>
+    </div>
     </div>
   </>
   )
